c57d61c3a419e7975bd1fe01c024ff22ee593372
Гюлумянц Борис Олегович <bgyulumyants@tsc.ts>
2019-11-18 14:54:54 +0300

    Some fixes

--- audit/audit_bkp.sh
+++ audit/audit_bkp.sh
@@ -8,12 +8,13 @@
 #   Date: 13.11.2019
 #
 #   How to use: You can run script manualy by <sid>adm
-#   You can schedule script once a day by adding it to the crontab 
-#   * * */1 * * <sid>adm /<path to script>/audit_bkp.sh > /<path to script>/audit_bkp.log 2>&1
+#   You can schedule script once a day by adding it to the crontab
+#   0 23 */1 * * <sid>adm /<path to script>/audit_bkp.sh > /<path to script>/audit_bkp.log 2>&1
 
 #   Set retention days for security audit logs
+
 delete_mode=1
-retention_days=90
+retention_days="+60"
 
 #   Set file parts
 preffix="audit_"
@@ -32,13 +33,14 @@ ArchiveLogs() {
         for ((i=1; i<=12; i++)); do
             if [ $i -lt 10 ];
             then
-                find ./ -type f \( -iname "${preffix}${j}${suffix}0${i}*" ! -iname "*.zip" \) | xargs -r zip -uv ${preffix}${j}${suffix}0${i}.zip
+                find ./ -type f \( -iname "${preffix}${j}${suffix}0${i}*" ! \( -iname "*.zip" -or -iname "*.tar.gz" \) \) | xargs -r zip -uv ${preffix}${j}${suffix}0${i}.zip
             else
-                find ./ -type f \( -iname "${preffix}${j}${suffix}${i}*" ! -iname "*.zip" \) | xargs -r  zip -uv ${preffix}${j}${suffix}${i}.zip
+                find ./ -type f \( -iname "${preffix}${j}${suffix}${i}*" ! \( -iname "*.zip" -or -iname "*.tar.gz" \) \) | xargs -r  zip -uv ${preffix}${j}${suffix}${i}.zip
             fi
         done
     done
 }
+
 
 DeleteLogs() {
     # For the test purpose this code only prints files needs to be removed. To productive run uncomment string
@@ -46,10 +48,10 @@ DeleteLogs() {
     #find ./ -type f \( -iname "${preffix}*" ! -iname "*.zip" \) -mtime $retention_days -delete
 }
 
-local_time
+
 echo $local_time
-if [delee_mode -eq 1]; 
-then 
+if [ $delete_mode -eq 1 ];
+then
     echo "Script run in delete mode"
 else
     echo "Script run in archive mode"
@@ -68,21 +70,25 @@ if [ $? -eq 0 ]; then
         rc2=$?
         if [ $rc1 -eq 0 ]; then
             cd /usr/sap/$SAPSYSTEMNAME/DVEBM*[0-9][0-9]/log
-            echo "Archiving logs from: $(pwd)"
-            ArchiveLogs
-            if [delee_mode -eq 1]; then 
-                echo "Deleting logs older than $retention_days days from:"
-                echo "$(pwd)"
-                DeleteLogs
+            if [ $? -eq 0 ]; then
+                echo "Archiving logs from: $(pwd)"
+                ArchiveLogs
+                if [ $delete_mode -eq 1 ]; then
+                    echo "Deleting logs older than $retention_days days from:"
+                    echo "$(pwd)"
+                    DeleteLogs
+                fi
             fi
         elif [ $rc2 -eq 0 ]; then
             cd /usr/sap/$SAPSYSTEMNAME/D[0-9][0-9]/log
-            echo "Archiving logs from: $(pwd)"
-            ArchiveLogs
-            if [delee_mode -eq 1]; then 
-                echo "Deleting logs older than $retention_days days from:"
-                echo "$(pwd)"
-                DeleteLogs
+            if [ $? -eq 0 ]; then
+                echo "Archiving logs from: $(pwd)"
+                ArchiveLogs
+                if [ $delete_mode -eq 1 ]; then
+                    echo "Deleting logs older than $retention_days days from:"
+                    echo "$(pwd)"
+                    DeleteLogs
+                fi
             fi
         fi
     done
